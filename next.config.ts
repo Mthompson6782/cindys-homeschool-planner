88def93690ccf9892d9bb217eadbd36b4b2a9980
Mthompson6782 <mthompson6782@gmail.com>
2026-07-18 08:36:49 -0500

    Mobile CSS fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow all localtunnel subdomains to load CSS/JS in dev mode
+  allowedDevOrigins: [
+    "*.loca.lt",
+    "loca.lt"
+  ]
 };
 
 export default nextConfig;
